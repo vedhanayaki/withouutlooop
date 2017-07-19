68b27d9e41650873b09b6bc9fba76cb0effe710f
vedhanayaki <vedhanayaki1996@gmail.com>
2017-07-19 01:57:28 -0700

    Add files via upload

---
+++ Withoutusingstring.java
@@ -0,0 +1,12 @@
+package player;
+
+public class Withoutusingstring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String a="dog";
+String b="cat";
+System.out.println(a+b);
+	}
+
+}
